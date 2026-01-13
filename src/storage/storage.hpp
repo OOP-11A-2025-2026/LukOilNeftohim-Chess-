8613ebc70e2f800e27462f569faefeac76162ed6
bananateo <teodorkamchev@gmail.com>
2026-01-13 19:36:38 +0200

    Здравейте друзиями

---
+++ src/storage/storage.hpp
@@ -0,0 +1,55 @@
+#ifndef STORAGE_HPP
+#define STORAGE_HPP
+
+
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <vector>
+
+void writefile( std::string filename,  std::string text)
+{
+    std::ofstream file(filename);
+
+    if (!file) {
+        std::cerr << "Failed to open file for writing.\n";
+        return;
+    }
+
+    file << text;
+    file.close();
+}
+
+ std::vector< std::string> readFile( std::string filename,  std::string& game_specs)
+{
+     std::vector< std::string> moves; 
+     std::string tmp = "";
+     std::ifstream file(filename);
+    if (!file) 
+    {
+        std::cerr << "Failed to open file for writing.\n";
+        return {};
+    }
+    
+    do
+    {
+        if(tmp[0] != '[')
+        getline(file, tmp);
+        game_specs += tmp + '\n';
+    }while(tmp[0] == '[');
+
+    while (file >> tmp) {
+
+        if (isdigit(tmp[0]) || tmp[0] == '.')
+            continue;
+
+        if (tmp == "1-0" || tmp == "0-1" || tmp == "1/2-1/2")
+            break;
+
+        moves.push_back(tmp);
+    }
+
+    return moves;
+}
+
+#endif
